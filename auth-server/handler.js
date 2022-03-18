ae99e7c6be90638f679b43e37ee46c9a0072d004
Kevin Hurdt <93124068+khurdt@users.noreply.github.com>
2022-03-18 17:09:42 -0500

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -22,8 +22,8 @@ const credentials = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://khurdt.github.io/meet-app'],
-  javascript_origins: ["https://khurdt.github.io", "http://localhost:3000"],
+  redirect_uris: ['https://khurdt.github.io/meet-app/'],
+  javascript_origins: ['https://khurdt.github.io', 'http://localhost:3000'],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
