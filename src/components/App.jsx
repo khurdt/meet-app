8b221db2d91354d2fe67c141b96b3c59ce87e01b
khurdt <joshuahurdt@gmail.com>
2022-04-22 03:49:49 -0500

    added router-dom and navbar footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import '../nprogress.css';
-import { Container, Row, Navbar } from 'react-bootstrap';
+import { Container, Row, Navbar, Nav } from 'react-bootstrap';
 import './App.css';
 import NumberOfEvents from './NumberOfEvents';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import WelcomeScreen from './WelcomeScreen';
 import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
-import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Redirect, Link } from 'react-router-dom';
 import { WarningAlert } from './Alert';
+import Charts from './Charts';
 
 class App extends Component {
   constructor() {
@@ -36,7 +37,7 @@ class App extends Component {
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get('code');
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
+    if (window.location.href.startsWith('http://localhost')) {
       getEvents().then((events) => {
         if (this.mounted) {
           this.setState({
@@ -44,7 +45,17 @@ class App extends Component {
           });
         }
       });
-    };
+    } else {
+      if ((code || isTokenValid) && this.mounted) {
+        getEvents().then((events) => {
+          if (this.mounted) {
+            this.setState({
+              events, locations: extractLocations(events), number: events.length, originalMaxEvents: events.length
+            });
+          }
+        });
+      };
+    }
   }
 
   componentWillUnmount() {
@@ -93,24 +104,46 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <Navbar style={{ backgroundColor: '#474242', height: '30px', margin: '0', padding: '0', zIndex: '100' }}>
-          <Navbar.Brand className='m-auto' style={{ color: '#00ffff', fontSize: '15px' }}>Meet</Navbar.Brand>
-        </Navbar>
-        <div style={{ height: '0px' }}>
-        <WarningAlert className='ml-auto' text={warningText} />
+      <Router>
+        <div className="App">
+          <Navbar style={{ backgroundColor: '#474242', height: '30px', margin: '0', padding: '0', zIndex: '100' }}>
+            <Navbar.Brand className='m-auto' style={{ color: '#00ffff', fontSize: '15px' }}>Meet</Navbar.Brand>
+          </Navbar>
+          <div style={{ height: '0px' }}>
+            <WarningAlert className='ml-auto' text={warningText} />
+          </div>
+
+          <Route exact path='/' render={() => {
+            return (
+              <div>
+                {isEventsLoaded ? (
+                  <Container fluid style={{ margin: '10px' }}>
+                    <Row className='justify-content-md-center'>
+                      <CitySearch locations={locations} updateEvents={this.updateEvents} number={number} />
+                      <NumberOfEvents updateEvents={this.updateEvents} suggestion={suggestion} number={number} originalMaxEvents={originalMaxEvents} events={events} />
+                      <EventList events={events} updateEvents={this.updateEvents} suggestion={suggestion} />
+                    </Row>
+                  </Container>) : (
+                  <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+                    getAccessToken={() => { getAccessToken() }} />)}
+              </div>
+            );
+          }} />
+
+          <Route path='/charts' render={() => {
+            return <Charts />
+          }} />
+
+          <div className='footer'>
+            <Navbar style={{ backgroundColor: '#1E2127', height: '56px', margin: '0', padding: '0', zIndex: '100' }}>
+              <Nav className='m-auto' style={{ backgroundColor: '#1E2127', paddingBottom: '20px' }}>
+                <Nav.Link style={{ color: 'white' }} href='/'>Home</Nav.Link>
+                <Nav.Link style={{ color: 'white' }} href='/charts'>Charts</Nav.Link>
+              </Nav>
+            </Navbar>
+          </div>
         </div>
-        {isEventsLoaded ? (
-          <Container fluid style={{ margin: '10px' }}>
-            <Row className='justify-content-md-center'>
-              <CitySearch locations={locations} updateEvents={this.updateEvents} number={number} />
-              <NumberOfEvents updateEvents={this.updateEvents} suggestion={suggestion} number={number} originalMaxEvents={originalMaxEvents} events={events} />
-              <EventList events={events} updateEvents={this.updateEvents} suggestion={suggestion} />
-            </Row>
-          </Container>) : (
-          <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
-            getAccessToken={() => { getAccessToken() }} />)}
-      </div>
+      </Router>
     );
   }
 }
